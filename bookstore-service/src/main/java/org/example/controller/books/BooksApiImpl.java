c058d89bf5713a2b01e8ea5dcfd71352bc43fd04
akonovalova <A.Konovalova@compussplus.com>
2026-05-20 11:14:43 +0500

    fix: Внесены правки согласно ревью

--- bookstore-service/src/main/java/org/example/controller/books/BooksApiImpl.java
+++ bookstore-service/src/main/java/org/example/controller/books/BooksApiImpl.java
@@ -2,6 +2,7 @@ package org.example.controller.books;
 
 import org.example.model.Book;
 import org.example.api.BookApi;
+import org.example.model.Genre;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ import java.util.List;
 public class BooksApiImpl implements BookApi {
 
     @Override
-    public ResponseEntity<List<Book>> getBooks(Integer id, Integer authorId, Integer seriesId, Integer publisherId, String genre, String publisherName, Boolean isPopular, Boolean isNew, Integer limit, Integer offset, String sort, String order) {
+    public ResponseEntity<List<Book>> getBooks(Integer id, Integer authorId, Integer seriesId, Integer publisherId, Genre genre, String publisherName, Boolean isPopular, Boolean isNew, Integer limit, Integer offset, String sortBook, String order) {
         return ResponseEntity.ok(List.of());
     }
 }
