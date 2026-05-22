b00edc42f051337b3ad92ed6f3ec4fb011e19d63
akonovalova <A.Konovalova@compussplus.com>
2026-05-22 11:16:48 +0500

    Добавлены скрипты по созданию и заполнению таблиц базы данных

---
+++ bookstore-service/db/schemaInsert.sql
@@ -0,0 +1,54 @@
+INSERT INTO baskets (total_price, quantity_books, created_at, updated_at)
+VALUES
+    (59.40, 4, '2026-05-20 10:00:00', '2026-05-21 11:00:00'),
+    (405.00, 3, '2026-03-10 14:00:00', '2026-04-09 13:00:00'),
+    (6500.00, 5, '2026-02-13 12:00:00', '2026-05-08 09:00:00');
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+INSERT INTO users (basket_id, name, surname, middle_name, email, phone_number, password)
+VALUES
+    (1, 'Ivan', 'Ivanov', 'Ivanovich', 'email1@mail.ru', '+7952346670', crypt('password1', gen_salt('bf'))),
+    (2, 'Petr', 'Petrov', NULL, 'email2@mail.ru', '+7923579935', crypt('password2', gen_salt('bf')));
+
+INSERT INTO authors (name, surname, middle_name, pseudonym)
+VALUES
+    ('Aizek', 'Azimov', NULL, 'Aizek'),
+    ('Boris', 'Akunin', NULL, 'Boric');
+
+INSERT INTO publishers (description, country, created_at, updated_at)
+VALUES
+    ('Description1', 'USA', '1940-05-21 11:00:00', '2026-05-22 10:00:00'),
+    ('Description2', 'Canada', '1990-05-15 11:00:00', '2026-05-20 10:00:00');
+
+INSERT INTO publisher_series (publisher_id, name, created_at, updated_at)
+VALUES
+    (1, 'Harry Potter', '2000-05-21 11:00:00', '2026-04-14 10:00:00'),
+    (2, 'Foundation Universe', '1950-05-21 11:00:00', '2026-05-17 10:00:00');
+
+INSERT INTO books (title, author_id, genre, publisher_id, series_id, price,
+isbn, release_date, description, pages, weight, review_count, average_rating)
+VALUES
+    ('Harry Potter and the Chamber of Secrets', 2, 'DRAMA', 1, 1, 500.00, '234-324-234-43', '2001-05-21 11:00:00', 'Description1', 400, 300.00, 40, 5),
+    ('I''m robot', 1, 'NON-FICTION', 2, 2, 650.00, '213-231-679-67', '1950-05-21 11:00:00', 'Description2', 300, 300.00, 20, 5);
+
+INSERT INTO basket_detail (basket_id, book_id, price, quantity)
+VALUES
+    (1, 1, 500.00, 1),
+    (2, 2, 650.00, 1);
+
+INSERT INTO book_reviews (book_id, reviewer_id, text, rating, created_at, updated_at)
+VALUES
+    (1, 1, 'Good', 4, '2010-05-21 11:00:00','2015-06-23 11:34:00'),
+    (2, 2, 'Awesome', 5, '2020-10-21 12:00:00',NULL);
+
+INSERT INTO favorite_books (user_id, book_id)
+VALUES
+    (1, 1),
+    (2, NULL);
+
+INSERT INTO orders (user_id, recipient_name, created_at, updated_at)
+VALUES
+    (1, 'Ivan Ivanov', '2025-03-19 11:00:00', NULL),
+    (2, 'Ivan Petrov', '2026-09-19 20:00:00', NULL);
+
